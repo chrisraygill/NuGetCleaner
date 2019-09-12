99b4265b847f9a33906027ffb455a9fa5e6a1ac0
Christopher R. Gill <christopher.gill@microsoft.com>
2019-09-12 14:51:25 -0700

    added classic CLI tool functionality

--- NuGetCleaner/Program.cs
+++ NuGetCleaner/Program.cs
@@ -1,30 +1,23 @@
 ﻿using System;
 using System.Reflection;
 using System.IO;
+using System.Text;
 
 namespace NuGetCleaner
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
             try
             {
                 if (args.Length == 0)
                 {
-                    var versionString = Assembly.GetEntryAssembly()
-                                            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-                                            .InformationalVersion
-                                            .ToString();
-
-                    Console.WriteLine($"NuGetCleaner v{versionString}");
-                    Console.WriteLine("-------------");
-                    Console.WriteLine("\nUsage:");
-                    Console.WriteLine("  NuGetCleaner <message>");
-                    return;
+                    GeneralInfo();
                 }
-
-                if (args[0] == "clean")
+                else if (args[0] == "clean")
                 {
                     var setting = CheckDisableLastAccess();
 
@@ -53,13 +46,28 @@ namespace NuGetCleaner
                 }
                 else
                 {
-                    Console.Write(args[0] + " is not a recognized command");
+                    Console.WriteLine("\n" + args[0] + " is not a recognized command\n");
+                    CommandsSupported();
                 }
             }
             catch (System.Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        public static void GeneralInfo()
+        {
+            var versionString = Assembly.GetEntryAssembly()
+                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                        .InformationalVersion
+                        .ToString();
+
+            Console.WriteLine($"\nNuGetCleaner v{versionString}");
+            Console.WriteLine("-------------\n");
+            Console.WriteLine("Usage: nugetcleaner <command>\n");
+            CommandsSupported();
+            return;
         }
 
         public static int CheckDisableLastAccess()
@@ -179,6 +187,51 @@ namespace NuGetCleaner
             }
 
             return dt;
+        }
+
+        public static void CommandsSupported()
+        {
+            Console.WriteLine("Supported Commands:");
+            CommandAndDescriptionPrint("clean");
+            CommandAndDescriptionPrint("clean <path>");
+            Console.WriteLine("");
+        }
+
+        public static void CommandAndDescriptionPrint(string comm)
+        {
+            var commSpace = 18;
+
+            StringBuilder sb = new StringBuilder(" " + comm);
+
+            for(int i = 0; i < commSpace-comm.Length; i++)
+            {
+                sb.Append(" ");
+            }
+
+            sb.Append(CommandDescription(comm));
+
+            Console.WriteLine(sb);
+        }
+
+        public static string CommandDescription(string comm)
+        {
+            switch (comm)
+            {
+                case "clean":
+                    return "Searches default GPF path for packages " +
+                            "unused beyond time threshold and deletes them";
+                case "clean <path>":
+                    return "Searches specified GPF path for packages " +
+                            "unused beyond time threshold and deletes them";
+                //case "enable":
+                //    return "Enables last access timestamp updates" +
+                //            "(must be ran as administrator)";
+                //case "info":
+                //    return "Enables last access timestamp updates" +
+                //            "(must be ran as administrator)";
+                default:
+                    return "";
+            }
         }
     }
 }
