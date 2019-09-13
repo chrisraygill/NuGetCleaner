74645adf5e7256244ba686196ece3996495b3fc2
Christopher R. Gill <christopher.gill@microsoft.com>
2019-09-13 14:59:20 -0700

    a more official CLI tool

--- NuGetCleaner/Program.cs
+++ NuGetCleaner/Program.cs
@@ -1,73 +1,54 @@
 ﻿using System;
 using System.Reflection;
 using System.IO;
-using System.Text;
+using CommandLine;
+using System.ComponentModel;
 
 namespace NuGetCleaner
 {
     class Program
     {
 
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            try
+            if (CheckDisableLastAccess() != 2)
             {
-                if (args.Length == 0)
-                {
-                    GeneralInfo();
-                }
-                else if (args[0] == "clean")
-                {
-                    var setting = CheckDisableLastAccess();
-
-                    if (setting != 2)
-                    {
-                        Console.WriteLine("\nYour Last Access updates are not currently enabled so this tool will not work");
-                        Console.WriteLine("To enable Last Access updates, run powershell as administrator and input:\n");
-                        Console.WriteLine("  fsutil behavior set disablelastaccess 2\n");
-                        Console.WriteLine("Note: you may be asked to reboot for the settings change to take effect\n");
-
-                        return;
-                    }
-
-                    var dir = "C:\\Users\\" + Environment.UserName + "\\.nuget\\packages";
-
-                    if (args.Length > 1)
-                    {
-                        dir = args[1];
-                    }
-
-                    Console.Write("Enter max package age (Days): ");
-                    var maxDays = Convert.ToInt32(Console.ReadLine());
-
-                    SearchAndDestroy(dir, maxDays);
-                    //SearchAndPrint(dir, maxDays);
-                }
-                else
-                {
-                    Console.WriteLine("\n" + args[0] + " is not a recognized command\n");
-                    CommandsSupported();
-                }
+                Console.Write("\nYour Last Access updates are not currently enabled so this tool will not work. \n" +
+                    "To enable Last Access updates, run powershell as administrator and input:\n\n" +
+                    "\tfsutil behavior set disablelastaccess 2\n\n" +
+                    "You may be asked to reboot for the settings change to take effect\n\n" +
+                    "Note: Last usage time of packages will only be tracked once the setting is enabled.\n" +
+                    "As such, last package usage will only be determined from the enable date onward.\n");
+                return;
             }
-            catch (System.Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(options => Execute(options));
         }
 
-        public static void GeneralInfo()
+        public static void Execute(Options options)
         {
-            var versionString = Assembly.GetEntryAssembly()
-                        .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-                        .InformationalVersion
-                        .ToString();
 
-            Console.WriteLine($"\nNuGetCleaner v{versionString}");
-            Console.WriteLine("-------------\n");
-            Console.WriteLine("Usage: nugetcleaner <command>\n");
-            CommandsSupported();
-            return;
+            string Path;
+
+            if (options.Path == null)
+            {
+                Path = "C:\\Users\\" + Environment.UserName + "\\.nuget\\packages";
+            }
+            else
+            {
+                Path = options.Path;
+            }
+
+            int Days = options.Days;
+
+            if (options.DryRun)
+            {
+                SearchAndPrint(Path, Days);
+            }
+            else {
+                SearchAndDestroy(Path, Days);
+            }
         }
 
         public static int CheckDisableLastAccess()
@@ -89,21 +70,19 @@ namespace NuGetCleaner
             return setting;
         }
 
-        public static void SearchAndDestroy(string dir, int maxDays)
+        public static void SearchAndDestroy(string Path, int Days)
         {
             try
             {
-                Console.WriteLine(DateTime.Now);
-
                 Console.WriteLine("Deleted Packages: ");
 
-                foreach (string pkg in Directory.GetDirectories(dir))
+                foreach (string pkg in Directory.GetDirectories(Path))
                 {
                     foreach (string pkgVersion in Directory.GetDirectories(pkg))
                     {
                         var dirAge = DateTime.Now - RecursiveFindLAT(pkgVersion, DateTime.MinValue);
 
-                        if (dirAge.TotalDays >= maxDays)
+                        if (dirAge.TotalDays >= Days)
                         {
                             Console.WriteLine(pkgVersion);
                             RecursiveDelete(pkgVersion);
@@ -122,28 +101,53 @@ namespace NuGetCleaner
             }
         }
 
-        public static void SearchAndPrint(string dir, int maxDays)
+        public static void SearchAndDestroyDR(string Path, int Days)
         {
             try
             {
-                Console.WriteLine(DateTime.Now);
-
                 Console.WriteLine("Deleted Packages: ");
 
-                foreach (string pkg in Directory.GetDirectories(dir))
+                foreach (string pkg in Directory.GetDirectories(Path))
+                {
+                    foreach (string pkgVersion in Directory.GetDirectories(pkg))
+                    {
+                        var dirAge = DateTime.Now - RecursiveFindLAT(pkgVersion, DateTime.MinValue);
+
+                        if (dirAge.TotalDays >= Days)
+                        {
+                            Console.WriteLine(pkgVersion + " --- Last Access: " + RecursiveFindLAT(pkgVersion, DateTime.MinValue));
+                            RecursiveDelete(pkgVersion);
+
+                            if (Directory.GetDirectories(pkg).Length == 0)
+                            {
+                                Directory.Delete(pkg);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void SearchAndPrint(string Path, int Days)
+        {
+            try
+            {
+                Console.WriteLine("Deleted Packages: ");
+
+                foreach (string pkg in Directory.GetDirectories(Path))
                 {
                     foreach (string pkgVersion in Directory.GetDirectories(pkg))
                     {
 
                         var dirAge = DateTime.Now - RecursiveFindLAT(pkgVersion, DateTime.MinValue);
 
-                        if (dirAge.TotalDays >= maxDays)
+                        if (dirAge.TotalDays >= Days)
                         {
-                            Console.WriteLine(pkgVersion + " --- " + RecursiveFindLAT(pkgVersion, DateTime.MinValue) + " --- Deleted");
-                        }
-                        else
-                        {
-                            Console.WriteLine(pkgVersion + " --- " + RecursiveFindLAT(pkgVersion, DateTime.MinValue));
+                            Console.WriteLine(pkgVersion.Substring(Path.Length + 1) + " --- Last Access: " + RecursiveFindLAT(pkgVersion, DateTime.MinValue));
                         }
                     }
                 }
@@ -187,51 +191,6 @@ namespace NuGetCleaner
             }
 
             return dt;
-        }
-
-        public static void CommandsSupported()
-        {
-            Console.WriteLine("Supported Commands:");
-            CommandAndDescriptionPrint("clean");
-            CommandAndDescriptionPrint("clean <path>");
-            Console.WriteLine("");
-        }
-
-        public static void CommandAndDescriptionPrint(string comm)
-        {
-            var commSpace = 18;
-
-            StringBuilder sb = new StringBuilder(" " + comm);
-
-            for(int i = 0; i < commSpace-comm.Length; i++)
-            {
-                sb.Append(" ");
-            }
-
-            sb.Append(CommandDescription(comm));
-
-            Console.WriteLine(sb);
-        }
-
-        public static string CommandDescription(string comm)
-        {
-            switch (comm)
-            {
-                case "clean":
-                    return "Searches default GPF path for packages " +
-                            "unused beyond time threshold and deletes them";
-                case "clean <path>":
-                    return "Searches specified GPF path for packages " +
-                            "unused beyond time threshold and deletes them";
-                //case "enable":
-                //    return "Enables last access timestamp updates" +
-                //            "(must be ran as administrator)";
-                //case "info":
-                //    return "Enables last access timestamp updates" +
-                //            "(must be ran as administrator)";
-                default:
-                    return "";
-            }
         }
     }
 }
